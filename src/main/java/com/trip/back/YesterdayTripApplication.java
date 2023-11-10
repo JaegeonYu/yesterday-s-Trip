e87b1dce5473a91ec5df9ef4df288c21ee970bf8
유재건 <yjk9805@naver.com>
2023-11-10 11:36:35 +0900

    init: springboot init

---
+++ src/main/java/com/trip/back/YesterdayTripApplication.java
@@ -0,0 +1,13 @@
+package com.trip.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YesterdayTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YesterdayTripApplication.class, args);
+	}
+
+}
