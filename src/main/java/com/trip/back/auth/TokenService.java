010b1072c0066bf973df593083dfe91701422897
유재건 <yjk9805@naver.com>
2023-11-16 17:37:37 +0900

    feat: refresh split

---
+++ src/main/java/com/trip/back/auth/TokenService.java
@@ -0,0 +1,56 @@
+package com.trip.back.auth;
+
+import java.net.URLDecoder;
+import java.util.regex.Pattern;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trip.back.account.Account;
+import com.trip.back.account.AccountMapper;
+import com.trip.back.account.Role;
+import com.trip.back.security.Jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class TokenService {
+	private static final Pattern BEARER = Pattern.compile("^Bearer$", Pattern.CASE_INSENSITIVE);
+	private final TokenMapper tokenRepository;
+	private final AccountMapper accountRepository;
+	private final Jwt jwt;
+	public String refreshAccess(String token) {
+		String validToken = obtainToken(token);
+		if(validToken != null) {
+			 Jwt.Claims claims = jwt.verify(validToken);
+			 if(validToken.equals(tokenRepository.findByAccountId(claims.userKey).getRefresh())){
+				 Account account = accountRepository.findByEmail(claims.email);
+				 String apiToken = account.newApiToken(jwt,  new String[] {Role.USER.value()});
+				 return apiToken;
+			 }
+		}
+		throw new RuntimeException();
+	}
+	
+	private String obtainToken(String token) {
+		if(token != null) {
+			try {
+				token = URLDecoder.decode(token, "UTF-8");
+				String[] tokens = token.split(" ");
+				if(tokens.length == 2) {
+					String scheme = tokens[0];
+					String credentails = tokens[1];
+					return BEARER.matcher(scheme).matches() ? credentails : null;
+				}
+			}catch(Exception e) {
+				log.error("Jwt ObtainAuthorization Process error");
+			}
+		}
+		return null;
+	}
+}
